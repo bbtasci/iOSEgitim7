404cb2bb744582cb69902e793cf4987f585ce4fe
Baris Berkin Tasci <barisberkin.tasci@hotmail.com>
2020-12-23 12:08:26 +0300

    First screen is created.

---
+++ MyPersonalZone/MyPersonalZone/ViewControllers/CreateZoneViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPersonalZone
+//
+//  Created by Baris Berkin Tasci on 23.12.2020.
+//
+
+import UIKit
+
+class CreateZoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

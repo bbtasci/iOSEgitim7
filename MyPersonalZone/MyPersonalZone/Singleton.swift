d381c2374f4c1957c4385990a1674ca0f4faee5a
Baris Berkin Tasci <barisberkin.tasci@hotmail.com>
2020-12-23 13:34:03 +0300

    First page is done. Singleton object is added and will be called in second page.

---
+++ MyPersonalZone/MyPersonalZone/Singleton.swift
@@ -0,0 +1,22 @@
+//
+//  Singleton.swift
+//  MyPersonalZone
+//
+//  Created by Baris Berkin Tasci on 23.12.2020.
+//
+
+class Singleton {
+    static let shared = Singleton()
+    
+    var name: String = ""
+    var surname: String = ""
+    var id: String = ""
+    var birthday: String = ""
+    var country: String = ""
+    var state: String = ""
+    var phone: String = ""
+    var university: String = ""
+    var horoscope: String = ""
+    var favTeam: String = ""
+    var favPlayer: String = ""
+}
--- MyPersonalZone/MyPersonalZone/ViewControllers/CreateZoneViewController.swift
+++ MyPersonalZone/MyPersonalZone/ViewControllers/CreateZoneViewController.swift
@@ -9,11 +9,123 @@ import UIKit
 
 class CreateZoneViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var appInfoLabel: UILabel!
+    
+    @IBOutlet weak var createNameTF: UITextField!
+    @IBOutlet weak var createSurnameTF: UITextField!
+    @IBOutlet weak var createIdentificationTF: UITextField!
+    @IBOutlet weak var createBirthdayTF: UITextField!
+    @IBOutlet weak var createCountryTF: UITextField!
+    @IBOutlet weak var createStateTF: UITextField!
+    @IBOutlet weak var createPhoneTF: UITextField!
+    @IBOutlet weak var createUniversityTF: UITextField!
+    @IBOutlet weak var createHoroscopeTF: UITextField!
+    @IBOutlet weak var createFavTeamTF: UITextField!
+    @IBOutlet weak var createFavPlayerTF: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        prepareUICZ()
+    }
+    
+    func prepareUICZ() {
+        prepareLogo()
+        prepareAppName()
+        prepareAppInfoLabel()
+        prepareCreateNameTF()
+        prepareCreateSurnameTF()
+        prepareCreateIdentificationTF()
+        prepareCreateBirthdayTF()
+        prepareCreateCountryTF()
+        prepareCreateStateTF()
+        prepareCreatePhoneTF()
+        prepareCreateUniversityTF()
+        prepareCreateHoroscopeTF()
+        prepareCreateFavTeamTF()
+        prepareCreateFavPlayerTF()
+        prepareSaveButton()
     }
 
-
+    func prepareLogo() {
+        logoImageView.layer.cornerRadius = 5
+        logoImageView.layer.masksToBounds = true
+    }
+    func prepareAppName() {
+        appNameLabel.layer.cornerRadius = 5
+        appNameLabel.layer.masksToBounds = true
+    }
+    func prepareAppInfoLabel() {
+        appInfoLabel.layer.cornerRadius = 5
+        appInfoLabel.layer.masksToBounds = true
+    }
+    func prepareCreateNameTF() {
+        createNameTF.layer.cornerRadius = 5
+        createNameTF.layer.masksToBounds = true
+    }
+    func prepareCreateSurnameTF() {
+        createSurnameTF.layer.cornerRadius = 5
+        createSurnameTF.layer.masksToBounds = true
+    }
+    func prepareCreateIdentificationTF() {
+        createIdentificationTF.layer.cornerRadius = 5
+        createIdentificationTF.layer.masksToBounds = true
+    }
+    func prepareCreateBirthdayTF() {
+        createBirthdayTF.layer.cornerRadius = 5
+        createBirthdayTF.layer.masksToBounds = true
+    }
+    func prepareCreateCountryTF() {
+        createCountryTF.layer.cornerRadius = 5
+        createCountryTF.layer.masksToBounds = true
+    }
+    func prepareCreateStateTF() {
+        createStateTF.layer.cornerRadius = 5
+        createStateTF.layer.masksToBounds = true
+    }
+    func prepareCreatePhoneTF() {
+        createPhoneTF.layer.cornerRadius = 5
+        createPhoneTF.layer.masksToBounds = true
+    }
+    func prepareCreateUniversityTF() {
+        createUniversityTF.layer.cornerRadius = 5
+        createUniversityTF.layer.masksToBounds = true
+    }
+    func prepareCreateHoroscopeTF() {
+        createHoroscopeTF.layer.cornerRadius = 5
+        createHoroscopeTF.layer.masksToBounds = true
+    }
+    func prepareCreateFavTeamTF() {
+        createFavTeamTF.layer.cornerRadius = 5
+        createFavTeamTF.layer.masksToBounds = true
+    }
+    func prepareCreateFavPlayerTF() {
+        createFavPlayerTF.layer.cornerRadius = 5
+        createFavPlayerTF.layer.masksToBounds = true
+    }
+    func prepareSaveButton() {
+        saveButton.layer.cornerRadius = 5
+        saveButton.layer.masksToBounds = true
+    }
+    
+    
+    @IBAction func saveButtonTouched(_ sender: Any) {
+        Singleton.shared.name = (createNameTF.text) ?? ""
+        Singleton.shared.surname = (createSurnameTF.text) ?? ""
+        Singleton.shared.id = (createIdentificationTF.text) ?? ""
+        Singleton.shared.birthday = (createBirthdayTF.text) ?? ""
+        Singleton.shared.country = (createCountryTF.text) ?? ""
+        Singleton.shared.state = (createStateTF.text) ?? ""
+        Singleton.shared.phone = (createPhoneTF.text) ?? ""
+        Singleton.shared.university = (createUniversityTF.text) ?? ""
+        Singleton.shared.horoscope = (createHoroscopeTF.text) ?? ""
+        Singleton.shared.favTeam = (createFavTeamTF.text) ?? ""
+        Singleton.shared.favPlayer = (createFavPlayerTF.text) ?? ""
+    }
+    
+    
 }
 
